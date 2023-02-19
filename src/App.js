bb64608e40c8bdd249882af83c39ebacf121d4b7
SvitlanaUcraniana <ivanishynasvitlana@gmail.com>
2023-02-19 19:33:40 +0100

    Added the Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by Svitlana Ivanishyna and is
@@ -13,7 +14,8 @@ export default function App() {
           {" "}
           open-sourced on GitHub
         </a>
-      </footer>
+      </footer>        
+      </div>
     </div>
   );
 }
