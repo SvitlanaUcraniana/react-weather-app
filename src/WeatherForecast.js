899e4bd358213457d1ddc3684c4f04ee0533ab7c
SvitlanaUcraniana <ivanishynasvitlana@gmail.com>
2023-02-26 21:17:04 +0100

    Extended the forecast for 5 days with looping

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,21 +4,27 @@ import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
-const [loaded, setLoaded] = useState(false);
-const [forecast, setForecast] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
 
   function handleResponse(response) {
     setLoaded(true);
-    setForecast(response.data.daily)
+    setForecast(response.data.daily);
   }
 
   if (loaded) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-           <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
